eb41c9813dff8020aa5939c51744c6fec9be794a
Thien Nguyen <tatthien.contact@gmail.com>
2022-08-24 15:30:30 +0700

    Move data placeholder to config file

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -11,28 +11,7 @@
 
 <script>
 import hljs from 'highlight.js'
-
-const PLACEHOLDER = `# Todo
-\`[ ] Your open task.\`
-\`[x] The completed one.\`
-
-You can create a @tag or @another-tag like this.
-
-Prioritize your task using !low !medium !high
-
-# You can use common markdown's syntax like:
-
-**bold**
-_italic_
-~~strike through~~
-
-> Stay hungry, stay foolish
-
-# Some ordered list
-1. First
-2. Second
-3. And the final
-`
+import dataPlaceholder from '../config/data-placeholder'
 
 const BRACKETS = new Map([
   ['[', ']'],
@@ -144,7 +123,7 @@ export default {
         return window.localStorage.getItem(this.id)
       }
 
-      return this.id === 'monday' ? PLACEHOLDER : ''
+      return this.id === 'monday' ? dataPlaceholder : ''
     },
     scrollSync () {
       this.highlightEl.scrollTop = this.editorEl.scrollTop
---
+++ src/config/data-placeholder.js
@@ -0,0 +1,21 @@
+export default `# Todo
+\`[ ] Your open task.\`
+\`[x] The completed one.\`
+
+You can create a @tag or @another-tag like this.
+
+Prioritize your task using !low !medium !high
+
+# You can use common markdown's syntax like:
+
+**bold**
+_italic_
+~~strike through~~
+
+> Stay hungry, stay foolish
+
+# Some ordered list
+1. First
+2. Second
+3. And the final
+`

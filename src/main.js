1dc2d7c4dded176965f09faeb0fe508dfe719f2d
Thien Nguyen <tatthien.contact@gmail.com>
2022-08-24 15:28:03 +0700

    Remove webfontloader

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import WebFontLoader from 'webfontloader'
 import 'modern-normalize'
 import './styles/app.scss'
 import './styles/theme.scss'
-
-WebFontLoader.load({
-  google: {
-    families: ['IBM Plex Mono:400,400i,600,700,700i']
-  }
-})
 
 Vue.config.productionTip = false
 
